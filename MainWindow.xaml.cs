dce2a5c627dfa6b177c626232cc379476e869a79
danstorek <50276516+danstorek@users.noreply.github.com>
2021-09-19 15:11:03 +0200

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace _2DFightingGame
+{
+    public partial class MainWindow : Window
+    {
+        List<Updatable> aktivni_projektily = new List<Updatable>();
+
+        bool vlevo = false;
+        bool vpravo = false;
+        bool smer = true;
+        bool skokTrigger = false;
+        bool veVzduchu = false;
+        bool skok = false;
+        bool utok1 = false;
+        bool utok2 = false;
+        DateTime cooldown = DateTime.Now;
+
+        int pohybX = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer gameTick = new DispatcherTimer();
+            gameTick.Interval = TimeSpan.FromMilliseconds(1000 / 60);
+            gameTick.Tick += GameTick_Tick;
+            gameTick.Start();
+        }
+
+        private void GameTick_Tick(object sender, EventArgs e)
+        {
+            Thickness pozice1 = postava1.Margin;
+
+            //Aktualizace aktivních projektilů ve hře
+            foreach (Updatable i in aktivni_projektily)
+            {
+                if (i.getAktivni()) i.Tick();
+                else Plocha.Children.Remove(i.ReturnImage());
+            }
+
+            //Útok 1 - hráč 1
+            if (utok1)
+            {
+                if (DateTime.Now > cooldown)
+                {
+                    Fireball fireball = new Fireball(postava1, smer);
+                    Plocha.Children.Add(fireball.ReturnImage());
+                    aktivni_projektily.Add(fireball);
+                    cooldown = DateTime.Now.AddMilliseconds(fireball.cooldown);
+                }
+
+            }
+
+            //Útok 2 - hráč 1
+            if (utok2)
+            {
+                if (DateTime.Now > cooldown)
+                {
+                    TNT sw = new TNT(postava1);
+                    Plocha.Children.Add(sw.ReturnImage());
+                    aktivni_projektily.Add(sw);
+                    cooldown = DateTime.Now.AddMilliseconds(sw.cooldown);
+                }
+            }
+            //Pohyb - hráč 1
+            if (vpravo)
+            {
+                if (pohybX < 30) pohybX += 3;
+                postava1.Source = new BitmapImage(new Uri("pack://application:,,,/imgs/chars/char1/right/1.png"));
+                smer = true;
+            }
+            else if (vlevo)
+            {
+                if (pohybX > -30) pohybX -= 3;
+                postava1.Source = new BitmapImage(new Uri("pack://application:,,,/imgs/chars/char1/left/1.png"));
+                smer = false;
+            }
+            else
+            {
+                if (pohybX > 0) pohybX -= 3;
+                if (pohybX < 0) pohybX += 3;
+            }
+
+            if (pohybX > 0)
+            {
+                if (pozice1.Left + pohybX < 1800) pozice1.Left += pohybX;
+            }
+            else
+            {
+                if (pozice1.Left + pohybX > -70) pozice1.Left += pohybX;
+            }
+
+            //Skok - hráč 1
+            if (skokTrigger)
+            {
+                if (!veVzduchu)
+                {
+                    veVzduchu = true;
+                    skok = true;
+                }
+            }
+            if (skok)
+            {
+                pozice1.Bottom += 20;
+                if (pozice1.Bottom >= 500) skok = false;
+            }
+
+            //Gravitace - hráč 1
+            if (veVzduchu)
+            {
+                if (!skok)
+                {
+                    pozice1.Bottom -= 25;
+                    if (pozice1.Bottom < 211)
+                    {
+                        pozice1.Bottom = 211;
+                        veVzduchu = false;
+                    }
+                }
+            }
+
+            postava1.Margin = pozice1;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    vlevo = true;
+                    break;
+                case Key.Right:
+                    vpravo = true;
+                    break;
+                case Key.Up:
+                    skokTrigger = true;
+                    break;
+                case Key.NumPad1:
+                    utok1 = true;
+                    break;
+                case Key.NumPad2:
+                    utok2 = true;
+                    break;
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    vlevo = false;
+                    break;
+                case Key.Right:
+                    vpravo = false;
+                    break;
+                case Key.Up:
+                    skokTrigger = false;
+                    break;
+                case Key.NumPad1:
+                    utok1 = false;
+                    break;
+                case Key.NumPad2:
+                    utok2 = false;
+                    break;
+            }
+        }
+    }
+}
---
+++ Projektil.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace _2DFightingGame
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    /// 
+
+    abstract class Updatable
+    {
+        bool aktivni = true;
+        public abstract void Tick();
+        public abstract Image ReturnImage();
+        protected void Neaktivni()
+        {
+            aktivni = false;
+        }
+        public bool getAktivni()
+        {
+            return aktivni;
+        }
+    }
+
+    class Fireball : Updatable
+    {
+        int rychlost;
+        Image img = new Image();
+        Image postava;
+
+        public readonly int cooldown = 100;
+
+        public Fireball(Image postava, bool smer)
+        {
+            this.postava = postava;
+
+            Thickness pozice = postava.Margin;
+            if (smer)
+            {
+                pozice.Left += 200;
+                pozice.Bottom += 210;
+                img.Source = new BitmapImage(new Uri("pack://application:,,,/imgs/attacks/fireball.png"));
+            }
+            else
+            {
+                pozice.Left -= 80;
+                pozice.Bottom += 210;
+                img.Source = new BitmapImage(new Uri("pack://application:,,,/imgs/attacks/fireballleft.png"));
+            }
+            img.Width = 91;
+            img.Height = 91;
+            img.VerticalAlignment = VerticalAlignment.Bottom;
+            img.HorizontalAlignment = HorizontalAlignment.Left;
+            img.Margin = pozice;
+
+            if (smer) rychlost = 40;
+            else rychlost = -40;
+        }
+
+        public override void Tick()
+        {
+            Thickness pozice = img.Margin;
+            pozice.Left += rychlost;
+            img.Margin = pozice;
+
+
+            //Označení výstřelu mimo obraz jako neaktivní
+            if (pozice.Left < -100 || pozice.Left > 2100) Neaktivni();
+        }
+
+        public override Image ReturnImage()
+        {
+            return img;
+        }
+    }
+
+    class TNT : Updatable
+    {
+        public readonly int cooldown = 1200;
+
+        Image img = new Image();
+        List<BitmapImage> animace = new List<BitmapImage>();
+        int cisloFrame = 0;
+
+        public TNT(Image postava)
+        {
+            Thickness pozice = postava.Margin;
+
+            img.Height = 371;
+            img.Width = 398;
+            img.Source = new BitmapImage(new Uri("pack://application:,,,/imgs/attacks/tnt/1.png"));
+            img.HorizontalAlignment = HorizontalAlignment.Left;
+            img.VerticalAlignment = VerticalAlignment.Bottom;
+            img.Margin = new Thickness(pozice.Left, pozice.Top, pozice.Right, 211);
+
+            animace.Add(new BitmapImage(new Uri("pack://application:,,,/imgs/attacks/tnt/1.png")));
+            animace.Add(new BitmapImage(new Uri("pack://application:,,,/imgs/attacks/tnt/10.png")));
+            animace.Add(new BitmapImage(new Uri("pack://application:,,,/imgs/attacks/tnt/20.png")));
+            animace.Add(new BitmapImage(new Uri("pack://application:,,,/imgs/attacks/tnt/30.png")));
+            animace.Add(new BitmapImage(new Uri("pack://application:,,,/imgs/attacks/tnt/40.png")));
+        }
+
+        public override void Tick()
+        {
+            if (getAktivni())
+            {
+                switch (cisloFrame)
+                {
+                    case 0: img.Source = animace[0]; break;
+                    case 15: img.Source = animace[1]; break;
+                    case 30: img.Source = animace[2]; break;
+                    case 45: img.Source = animace[3]; break;
+                    case 60: img.Source = animace[4]; break;
+                }
+
+                if (cisloFrame > 120)
+                {
+                    Neaktivni();
+                }
+                cisloFrame++;
+            }
+
+        }
+
+        public override Image ReturnImage()
+        {
+            return img;
+        }
+    }
+}
